fbe2d6ad5b022c54715c6a8862fa45bd60bf5034
Arpit verma <arpit.verma@bhavnacorps.com>
2022-08-22 16:54:39 +0530

    Angular Project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirstComComponent } from './first-com/first-com.component';
+import { SecondComComponent } from './second-com/second-com.component';
+import { NavCompComponent } from './nav-comp/nav-comp.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComComponent,
+    SecondComComponent,
+    NavCompComponent
   ],
   imports: [
     BrowserModule,
